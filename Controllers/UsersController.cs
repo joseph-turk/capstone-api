a0a74b3294355e5ecc0e0fe9f762566dbb7ed8ae
Joseph Turk <joseph.r.turk@gmail.com>
2018-12-09 10:25:37 -0600

    Return appropriate data when registering

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -76,8 +76,32 @@ namespace CapstoneApi.Controllers
 
             try
             {
-                _userService.Create(user, userDto.Password);
-                return Ok();
+                User newUser = _userService.Create(user, userDto.Password);
+
+                JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+                byte[] key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+                SecurityTokenDescriptor tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(new Claim[]
+                    {
+                        new Claim(ClaimTypes.Name, newUser.Id.ToString())
+                    }),
+                    Expires = DateTime.UtcNow.AddDays(7),
+                    SigningCredentials = new SigningCredentials(
+                        new SymmetricSecurityKey(key),
+                        SecurityAlgorithms.HmacSha256Signature
+                    )
+                };
+                SecurityToken token = tokenHandler.CreateToken(tokenDescriptor);
+                string tokenString = tokenHandler.WriteToken(token);
+
+                return Ok(new
+                {
+                    Id = newUser.Id,
+                    Email = newUser.Email,
+                    IsAdmin = newUser.IsAdmin,
+                    Token = tokenString
+                });
             }
             catch (AppException ex)
             {
