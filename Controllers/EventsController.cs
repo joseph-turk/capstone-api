7902889c2fb04491655a8538ed88c55db6f3cfb6
Joseph Turk <joseph.r.turk@gmail.com>
2018-12-09 18:03:00 -0600

    Enable updating all fields; add per-user route

--- Controllers/EventsController.cs
+++ Controllers/EventsController.cs
@@ -45,8 +45,54 @@ namespace CapstoneApi.Controllers
         {
             List<EventDto> allEventsDto = new List<EventDto>();
             List<Event> allEvents = await _context.Events
-                .Include(e => e.Registrations)
-                .ToListAsync();
+                    .Include(e => e.Registrations)
+                    .ToListAsync();
+
+            allEvents.ForEach(e =>
+            {
+                allEventsDto.Add(new EventDto
+                {
+                    Id = e.Id,
+                    Name = e.Name,
+                    Description = e.Description,
+                    Start = e.Start,
+                    End = e.End,
+                    Capacity = e.Capacity,
+                    ImageId = e.ImageId,
+                    ImageExtension = e.ImageExtension,
+                    IsMyEvent = false,
+                    Registrations = null,
+                    RegistrationCount = e.Registrations.Where(r => !r.IsWaitList).Count(),
+                    WaitListCount = e.Registrations.Where(r => r.IsWaitList).Count()
+                });
+            });
+
+            return Ok(allEventsDto);
+        }
+
+        [HttpGet("foruser")]
+        public async Task<ActionResult> GetAllForUser()
+        {
+            Guid userId;
+            Guid.TryParse(User.Identity.Name, out userId);
+            User user = _context.Users.Find(userId);
+
+            List<EventDto> allEventsDto = new List<EventDto>();
+            List<Event> allEvents;
+
+            if (user.IsAdmin)
+            {
+                allEvents = await _context.Events
+                    .Include(e => e.Registrations)
+                    .ToListAsync();
+            }
+            else
+            {
+                allEvents = await _context.Events
+                    .Where(e => e.CreatedBy == user)
+                    .Include(e => e.Registrations)
+                    .ToListAsync();
+            }
 
             allEvents.ForEach(e =>
             {
@@ -196,7 +242,7 @@ namespace CapstoneApi.Controllers
 
         [HttpPut("{id}")]
         [Authorize]
-        public IActionResult Update(Guid id, Event item)
+        public IActionResult Update(Guid id)
         {
             User editedBy = _context.Users.Find(Guid.Parse(this.User.Identity.Name));
             Event existingEvent = _context.Events
@@ -209,14 +255,70 @@ namespace CapstoneApi.Controllers
                 return BadRequest();
             }
 
-            existingEvent.Name = item.Name;
-            existingEvent.Description = item.Description;
-            existingEvent.Start = item.Start;
-            existingEvent.End = item.End;
-            existingEvent.Capacity = item.Capacity;
+            if (Request.HasFormContentType)
+            {
+                IFormCollection form = Request.Form;
 
-            _context.Events.Update(existingEvent);
-            _context.SaveChanges();
+                // Update Fields
+                existingEvent.Name = form["name"];
+                existingEvent.Description = form["description"];
+                existingEvent.Start = Convert.ToDateTime(form["start"]).ToUniversalTime();
+                existingEvent.End = Convert.ToDateTime(form["end"]).ToUniversalTime();
+                existingEvent.Capacity = int.Parse(form["capacity"]);
+
+                // Update Image
+                IFormFile image = form.Files.FirstOrDefault();
+                if (image != null)
+                {
+                    Guid imageId = Guid.NewGuid();
+
+                    string uploads = Path.Combine(_hostingEnvironment.WebRootPath, "uploads");
+                    string imageDir = Path.Combine(uploads, imageId.ToString());
+                    string oldDir = Path.Combine(uploads, existingEvent.ImageId.ToString());
+                    if (!Directory.Exists(uploads)) Directory.CreateDirectory(uploads);
+
+                    // Delete existing images
+                    if (Directory.Exists(oldDir)) Directory.Delete(oldDir, true);
+
+                    //Create new image directory
+                    Directory.CreateDirectory(imageDir);
+
+                    // Get file extension
+                    string fileExtension = Path.GetExtension(image.FileName);
+
+                    Image<Rgba32> resizedImage = imageService.CreateThumbnail(image);
+                    Image<Rgba32> heroImage = imageService.CreateHero(image);
+
+                    // Set paths for saving files
+                    string uniqueFileName = String.Concat("full", fileExtension);
+                    string thumbnailFileName = String.Concat("thumbnail", fileExtension);
+                    string heroFileName = String.Concat("hero", fileExtension);
+                    string filePath = Path.Combine(imageDir, uniqueFileName);
+                    string thumbnailPath = Path.Combine(imageDir, thumbnailFileName);
+                    string heroPath = Path.Combine(imageDir, heroFileName);
+
+                    // Save full-size file
+                    using (FileStream fileStream = new FileStream(filePath, FileMode.Create))
+                    {
+                        image.CopyTo(fileStream);
+                        fileStream.Flush();
+                    }
+
+                    // Save thumbnail and hero
+                    resizedImage.Save(thumbnailPath);
+                    heroImage.Save(heroPath);
+
+                    existingEvent.ImageId = imageId;
+                    existingEvent.ImageExtension = fileExtension;
+                }
+
+                _context.Events.Update(existingEvent);
+                _context.SaveChanges();
+            }
+            else
+            {
+                return BadRequest();
+            }
 
             return NoContent();
         }
