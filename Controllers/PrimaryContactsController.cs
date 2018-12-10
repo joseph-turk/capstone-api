12cbd642c0da85779d583b3a83029335cfdeb389
Joseph Turk <joseph.r.turk@gmail.com>
2018-12-10 13:58:07 -0600

    Restrict view of registrants and primary contacts

--- Controllers/PrimaryContactsController.cs
+++ Controllers/PrimaryContactsController.cs
@@ -5,11 +5,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using CapstoneApi.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
 
 namespace CapstoneApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class PrimaryContactsController : ControllerBase
     {
         private readonly CapstoneContext _context;
@@ -22,9 +24,41 @@ namespace CapstoneApi.Controllers
         [HttpGet]
         public ActionResult<List<PrimaryContact>> GetAll()
         {
-            return _context.PrimaryContacts
-                .Include(pc => pc.Registrations)
-                .ToList();
+            Guid userId;
+            Guid.TryParse(User.Identity.Name, out userId);
+
+            User user = _context.Users.Find(userId);
+            if (user == null) return BadRequest();
+
+            if (user.IsAdmin)
+            {
+                return _context.PrimaryContacts
+                    .Include(pc => pc.Registrations)
+                    .ToList();
+            }
+            else
+            {
+                List<Event> events = _context.Events
+                    .Where(e => e.CreatedBy.Id == user.Id)
+                    .Include(e => e.Registrations)
+                    .ThenInclude(r => r.PrimaryContact)
+                    .ToList();
+
+                List<PrimaryContact> primaryContacts = new List<PrimaryContact>();
+
+                events.ForEach(e =>
+                {
+                    e.Registrations.ForEach(r =>
+                    {
+                        if (!primaryContacts.Any(pc => pc.Id == r.PrimaryContact.Id))
+                        {
+                            primaryContacts.Add(r.PrimaryContact);
+                        }
+                    });
+                });
+
+                return primaryContacts;
+            }
         }
     }
 }
--- Controllers/RegistrantsController.cs
+++ Controllers/RegistrantsController.cs
@@ -5,11 +5,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using CapstoneApi.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
 
 namespace CapstoneApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class RegistrantsController : ControllerBase
     {
         private readonly CapstoneContext _context;
@@ -22,9 +24,42 @@ namespace CapstoneApi.Controllers
         [HttpGet]
         public ActionResult<List<Registrant>> GetAll()
         {
-            return _context.Registrants
-                .Include(r => r.Registrations)
-                .ToList();
+            Guid userId;
+            Guid.TryParse(User.Identity.Name, out userId);
+
+            User user = _context.Users.Find(userId);
+            if (user == null) return BadRequest();
+
+            if (user.IsAdmin)
+            {
+                return _context.Registrants
+                    .Include(r => r.Registrations)
+                    .ToList();
+            }
+            else
+            {
+                List<Event> events = _context.Events
+                    .Where(e => e.CreatedBy.Id == user.Id)
+                    .Include(e => e.Registrations)
+                    .ThenInclude(r => r.Registrant)
+                    .ToList();
+
+                List<Registrant> registrants = new List<Registrant>();
+
+                events.ForEach(e =>
+                {
+                    e.Registrations.ForEach(r =>
+                    {
+                        if (!registrants.Any(reg => reg.Id == r.Registrant.Id))
+                        {
+                            registrants.Add(r.Registrant);
+                        }
+                    });
+                });
+
+                return registrants;
+            }
+
         }
     }
 }
